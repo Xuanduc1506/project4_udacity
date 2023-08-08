4135262f51ea07045ee4d74bb079db51d0582ca2
xuanduc1506 <nxduc150600@gmail.com>
2023-08-08 21:37:17 +0700

    add ciclerci

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ducnx1506/udaproject4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udaproject4 \
+        --image=ducnx1506/udaproject4 --port=80 \
+        --labels app=udaproject4
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udaproject4 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ducnx1506/udaproject4"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag udaproject4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
